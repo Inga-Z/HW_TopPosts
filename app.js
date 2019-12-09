3ddff3de9c72f6c3d22cdcad288542102857c054
Inga-Z <z_inga@mail.ru>
2019-12-09 20:34:18 +0300

    like/dislike added

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ const server = express();
 server.use(express.json());
 server.use(cors());
 
+function findPostIndexById(id) {
+    return posts.findIndex(o => o.id === id);
+   }
+
 server.get('/posts', (req, res) => {
     res.send(posts);
 });
@@ -29,7 +33,7 @@ server.post('/posts', (req, res) => {
         return;
     }
 
-    const index = posts.findIndex(o => o.id === body.id);
+    const index = findPostIndexById(body.id);
     if (index === -1) {
         res.status(404).send(errorNotFound);
         return;
@@ -40,9 +44,9 @@ server.post('/posts', (req, res) => {
 });
 
 server.delete('/posts/:id', (req, res) =>{
-    const id = Number(req.params.id)
+    const id = Number(req.params.id);
 
-    const index = posts.findIndex(o => o.id === id);
+    const index = findPostIndexById(id);
     if (index === -1) {
         res.status(404).send(errorNotFound);
         return;
@@ -50,6 +54,32 @@ server.delete('/posts/:id', (req, res) =>{
 
     posts.splice(index, 1);
     res.send(posts);
-})
+});
+
+server.post('/posts/:id/likes', (req, res) => {
+    const id = Number(req.params.id);
+
+    const index = findPostIndexById(id);
+    if (index === -1) {
+        res.status(404).send(errorNotFound);
+        return;
+    }
+
+    posts[index].likes++;
+    res.send(posts[index]);
+});
+
+server.delete('/posts/:id/likes', (req, res) => {
+    const id = Number(req.params.id);
+
+    const index = findPostIndexById(id);
+    if (index === -1) {
+        res.status(404).send(errorNotFound);
+        return;
+    }
+
+    posts[index].likes--;
+    res.send(posts[index]);
+});
 
 server.listen(process.env.PORT || 9999);
